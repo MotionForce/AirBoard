bb944e2238e3a9081e0e7cf9df558375f74f90a6
MotionForce <74267742+MotionForce@users.noreply.github.com>
2025-01-26 04:48:54 -0500

    much cleaner UI (code and visual)

--- mchacks2025web/src/lib/components/Tutorial.svelte
+++ mchacks2025web/src/lib/components/Tutorial.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
     import Keyboard from "$lib/components/svg/Keyboard.svelte";
     import Hand from "$lib/components/svg/Hand.svelte";
-    import {onMount} from "svelte";
     import {fade} from "svelte/transition";
     import {messages, connectWebSocket, sendMessage} from "$lib/websocket";
     import {onMount} from "svelte";
@@ -16,7 +15,7 @@
     let handContainer: HTMLElement = $state(null);
 
     let tutorial_ended = $state(false);
-    let setup_phase = $state(true);
+    let setup_phase = $state(false);
 
     $effect(() => {
         if (tutorial_ended) {
@@ -43,7 +42,7 @@
     onMount(() => {
         // Access webcam
         const video = document.getElementById('webcam');
-        navigator.mediaDevices.getUserMedia({ video: true })
+        navigator.mediaDevices.getUserMedia({video: true})
             .then(stream => {
                 video.srcObject = stream;
                 startFrameCapture();
@@ -64,69 +63,68 @@
 </script>
 
 {#if !tutorial_ended}
-    <div transition:fade>
-        <div class="p-4 py-8 border-2 rounded-3xl overflow-clip bg-gradient-to-tr from-blue-300  to-pink-300">
-            <div>
-                <Keyboard color="#394955"/>
-            </div>
-            {#if !setup_phase}
-                <div class="absolute left-0 right-0 flex flex-col py-96 mb-[-37%] space-y-6 bottom-0 items-center justify-center align-middle">
-                    <div class="rounded-2xl border-4 p-4 backdrop-blur-xl backdrop-brightness-[1.3] animate-title {!loaded ? 'pause' : ''}">
-                        <h3 class="text-3xl">Place your fingers over the middle row, palms about 2cm in the air.</h3>
-                    </div>
-                    <div class="mt-3 animate-button {!loaded ? 'pause' : ''}">
-                        <button class="border-2 p-4 text-xl rounded-md bg-white" onclick={() => setup_phase=true}>
-                            Ready!
-                        </button>
-                        <div class="text-white drop-shadow-2xl flex flex-col items-center space-y-1 animate-arrow {!loaded ? 'pause' : ''}">
-                            <img src="/svg/up-arrow-svgrepo-com.svg" alt="up arrow" width="32" class="invert">
-                            <p class="">Click me!</p>
-                        </div>
+    <div class="p-16 border-2 rounded-3xl bg-gradient-to-tr from-blue-300  to-pink-300 w-full flex align-middle items-center justify-center z-10"
+         transition:fade>
+        <div>
+            <Keyboard color="#394955"/>
+        </div>
+        {#if !setup_phase}
+            <div class="z-30 absolute left-0 right-0 flex flex-col py-96 mb-[-37%] space-y-6 bottom-0 items-center justify-center align-middle">
+                <div class="rounded-2xl border-4 p-4 backdrop-blur-xl backdrop-brightness-[1.3] animate-title {!loaded ? 'pause' : ''}">
+                    <h3 class="text-3xl">Place your fingers over the middle row, palms about 2cm in the air.</h3>
+                </div>
+                <div class="mt-3 animate-button {!loaded ? 'pause' : ''}">
+                    <button class="border-2 p-4 text-xl rounded-md bg-white" onclick={() => setup_phase=true}>
+                        Ready!
+                    </button>
+                    <div class="text-white drop-shadow-2xl flex flex-col items-center space-y-1 animate-arrow {!loaded ? 'pause' : ''}">
+                        <img src="/svg/up-arrow-svgrepo-com.svg" alt="up arrow" width="32" class="invert">
+                        <p class="">Click me!</p>
                     </div>
                 </div>
-            {:else}
-                <div class="absolute left-0 right-0 flex flex-col py-96 mb-[-37%] space-y-6 bottom-0 items-center justify-center align-middle">
-                    <div class="rounded-2xl border-4 p-4 backdrop-blur-xl backdrop-brightness-[1.3] {!loaded ? 'pause' : ''}">
-                        <h3 class="text-3xl">Press the key <span
-                                class="text-4xl bg-white p-2 rounded-md">{current_key}</span> on your keyboard, then
-                            click on the button below.</h3>
-                    </div>
-                    <div class="mt-3 animate-button {!loaded ? 'pause' : ''}">
-                        <button class="border-2 p-4 text-xl rounded-md bg-white" onclick={() => {
+            </div>
+        {:else}
+            <div class="z-30 absolute left-0 right-0 flex flex-col py-96 mb-[-37%] space-y-6 bottom-0 items-center justify-center align-middle">
+                <div class="rounded-2xl border-4 p-4 backdrop-blur-xl backdrop-brightness-[1.3]">
+                    <h3 class="text-3xl">Press the key <span
+                            class="text-4xl bg-white p-2 rounded-md">{current_key}</span> on your keyboard, then
+                        click on the button below.</h3>
+                </div>
+                <div class="mt-3" transition:fade>
+                    <button class="border-2 p-4 text-xl rounded-md bg-white" onclick={() => {
                             if (required_keys.indexOf(current_key) === required_keys.length - 1) {
                                 tutorial_ended = true;
                             } else {
                                 current_key = required_keys[required_keys.indexOf(current_key) + 1];
                             }
                         }}>
-                            Next
-                        </button>
-                        <div class="text-white
+                        Next
+                    </button>
+                    <div class="text-white
                         drop-shadow-2xl flex flex-col items-center space-y-1 animate-arrow {!loaded ? 'pause' : ''}">
-                            <img src="/svg/up-arrow-svgrepo-com.svg" alt="up arrow" width="32" class="invert">
-                            <p class="">Click me
-                            </p>
-                        </div>
+                        <img src="/svg/up-arrow-svgrepo-com.svg" alt="up arrow" width="32" class="invert">
+                        <p class="">Click me
+                        </p>
                     </div>
                 </div>
-            {/if}
-        </div>
+            </div>
+        {/if}
+    </div>
 
-        <div bind:this={handContainer} class="animate-hand">
-            <div class="hand">
-                <div class="absolute !scale-150 flex flex-row gap-64">
-                    <div class="drop-shadow-2xl">
-                        <Hand color="#394955" distance_between="400px" left={true}/>
-                    </div>
-                    <div class="drop-shadow-2xl">
-                        <Hand color="#394955" distance_between="400px" left={false}/>
-                    </div>
+    <div bind:this={handContainer} class="animate-hand absolute z-20" transition:fade>
+        <div class="hand">
+            <div class="!scale-150 flex flex-row gap-64">
+                <div class="drop-shadow-2xl">
+                    <Hand color="#394955" distance_between="400px" left={true}/>
+                </div>
+                <div class="drop-shadow-2xl">
+                    <Hand color="#394955" distance_between="400px" left={false}/>
                 </div>
             </div>
         </div>
     </div>
 {:else}
-    <div transition:fade>
+    <div class="w-full" transition:fade>
         <video class="border-2 rounded-3xl w-full h-full overflow-clip bg-black" width="640" height="480"
                autoplay={true}
                bind:this={videoRef}></video>
@@ -216,7 +214,7 @@
     }
 
     .hand {
-        transform: translate(23%, -3rem);
+        transform: translate(36rem, 40rem);
     }
 
     .animate-hand {
@@ -229,6 +227,10 @@
 
     .animate-button {
         animation: appearButton 14s infinite;
+    }
+
+    .animate-button-fast {
+        animation: appearButton 4s infinite;
     }
 
     .animate-arrow {
